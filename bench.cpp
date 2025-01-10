b19b9c58c924c359f936ac10a5f5d33dd0d2769b
Julien <pgyygp090@gmail.com>
2025-01-10 14:47:06 +0100

    removed unused function

--- bench.cpp
+++ bench.cpp
@@ -4,7 +4,6 @@ int main() {
   std::string x = "21685454520057057454344565656454";
   std::string x2 = "816854545200570574354534343";
   std::cout << addteste(x, x2) << "\n";
-  std::cout << addint(x, x2) << "\n";
   for (int i = 0; i < 100000; ++i) {
     addteste(x, x2);
   };
