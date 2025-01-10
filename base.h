f2fec57701476e8fef1cc64c6f5be7474fa15f6c
Julien <pgyygp090@gmail.com>
2025-01-10 14:57:27 +0100

    added base.h

---
+++ base.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+bool is_greater(std::string &x, std::string &x2) {
+  const unsigned int n = x.length();
+  const unsigned int n2 = x2.length();
+  unsigned int val1;
+  unsigned int val2;
+  if (n == n2) {
+    for (unsigned int i = 0; i < n; ++i) {
+      val1 = int(x[i]);
+      val2 = int(x2[i]);
+      if (val1 != val2) {
+        if (val1 > val2) {
+          return 1;
+        } else {
+          return 0;
+        };
+      };
+    };
+  } else if (n > n2) {
+    return 1;
+  } else {
+    return 0;
+  };
+  return 1;
+};
